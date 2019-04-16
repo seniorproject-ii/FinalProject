3d2b808ebc9a0f4c2bb95e40572846c352f9f9e7
Ashok <ashokkafle96@gmail.com>
2019-04-16 17:36:24 -0400

    full authentication setup with firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCnPQP8EBUxLOrtt9Rd3e0oh1RMKZE0yxc",
+    authDomain: "subtracker-a0303.firebaseapp.com",
+    databaseURL: "https://subtracker-a0303.firebaseio.com",
+    projectId: "subtracker-a0303",
+    storageBucket: "subtracker-a0303.appspot.com",
+    messagingSenderId: "341711558431"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
